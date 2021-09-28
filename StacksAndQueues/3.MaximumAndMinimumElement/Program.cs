048a9d8f49d3b9188818f3eb4dd25ecf0dd1ff70
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-09-28 14:04:29 +0300

    Add files via upload

---
+++ StacksAndQueues/3.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < lines; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int action = input[0];
+
+                if (action == 1)
+                {
+                    int num = input[1];
+                    stack.Push(num);
+                }
+                else if (action == 2)
+                {
+                    stack.Pop();
+                }
+                else if (action == 3)
+                {
+                    if (stack.Count == 0)
+                    {
+                        continue;
+                    }
+                    foreach (var item in stack)
+                    {
+                        if (item > maxNum)
+                        {
+                            maxNum = item;
+                        }
+                    }
+                    Console.WriteLine(maxNum);
+                }
+                else if (action == 4)
+                {
+                    if (stack.Count == 0)
+                    {
+                        continue;
+                    }
+                    foreach (var item in stack)
+                    {
+                        if (item < minNum)
+                        {
+                            minNum = item;
+                        }
+                    }
+                    Console.WriteLine(minNum);
+                }
+            }
+            Console.WriteLine(string.Join(", ",stack));
+        }
+    }
+}
---
+++ StacksAndQueues/7.TruckTour/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPumps = int.Parse(Console.ReadLine());
+            Queue<int[]> queue = new Queue<int[]>();
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                queue.Enqueue(input);
+
+            }
+
+            int index = 0;
+
+            while (true)
+            {
+                int tank = 0;
+                foreach (var item in queue)
+                {
+                    int gas = item[0];
+                    int distance = item[1];
+
+                    tank += gas;
+                    tank -= distance;
+
+                    if (tank < 0)
+                    {
+                        int[] currentElem = queue.Dequeue();
+                        queue.Enqueue(currentElem);
+                        index++;
+                        break;
+                    }
+                }
+                if (tank>=0)
+                {
+                    Console.WriteLine(index);
+                    break;
+                }
+            }
+
+        }
+    }
+}
