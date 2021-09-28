aab96f0712bb5adf87d94ed3211a1bb45c933c3d
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-09-28 13:59:53 +0300

    Add files via upload

---
+++ 5.FashionBoutique/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cloths = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+            int racks = 1;
+            Stack<int> stack = new Stack<int>();
+
+            foreach (var item in cloths)
+            {
+                stack.Push(item);
+            }
+
+            int currentSum = 0;
+
+            foreach (var item in stack)
+            {
+                if (currentSum + item <= capacity)
+                {
+                    currentSum += item;
+                    
+                }
+                else
+                {
+                    currentSum = item;
+                    racks++;
+                }
+            }
+            Console.WriteLine(racks);
+        }
+    }
+}
