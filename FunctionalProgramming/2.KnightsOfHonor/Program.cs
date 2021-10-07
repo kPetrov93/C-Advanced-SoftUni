299b8645fb1dee3c136f8a4519afe03d303a8fe9
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-07 10:27:35 +0300

    Add files via upload

---
+++ FunctionalProgramming/2.KnightsOfHonor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = input => Console.WriteLine("Sir "+string.Join(Environment.NewLine + "Sir ", input));
+
+            string[] input = Console.ReadLine().Split();
+
+            print(input);
+        }
+    }
+}
