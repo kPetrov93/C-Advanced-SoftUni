94b2b851124606a51617886b322a7a23667edb82
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-07 10:26:49 +0300

    Add files via upload

---
+++ FunctionalProgramming/1.ActionPoint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = input => Console.WriteLine(string.Join(Environment.NewLine, input));
+
+            string[] input = Console.ReadLine().Split();
+
+            print(input);
+        }
+    }
+}
