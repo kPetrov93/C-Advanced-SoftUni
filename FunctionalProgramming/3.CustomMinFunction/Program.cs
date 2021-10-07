bfb6abfafaacfb85e3b0aa53079e68e6b293c49f
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-07 10:27:47 +0300

    Add files via upload

---
+++ FunctionalProgramming/3.CustomMinFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> min = numbers =>
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var item in numbers)
+                {
+                    if (item < minValue)
+                    {
+                        minValue = item;
+                    }
+                }
+
+                return minValue;
+            };
+
+            Console.WriteLine(min(Console.ReadLine().Split().Select(int.Parse).ToArray()));
+        }
+    }
+}
