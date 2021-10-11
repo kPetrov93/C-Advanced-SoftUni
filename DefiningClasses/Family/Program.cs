cdad8a1a33e37669926e08b5132a493afd050b85
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-11 11:16:20 +0300

    Add files via upload

---
+++ DefiningClasses/Family/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+
+                family.AddMember(person);
+            }
+
+            var sortedPeople = family.People.Where(x => x.Age > 30).OrderBy(x => x.Name);
+
+            foreach (var item in sortedPeople)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+        }
+    }
+}
