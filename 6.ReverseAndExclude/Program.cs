c0243441481ced6f2c14122221a95b29daa93fa1
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-13 10:49:43 +0300

    Add files via upload

---
+++ 6.ReverseAndExclude/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int n = int.Parse(Console.ReadLine());
+
+            nums.Reverse();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] % n == 0)
+                {
+
+                }
+                else
+                {
+                    Console.Write(nums[i] + " ");
+                }
+            }
+        }
+    }
+}
