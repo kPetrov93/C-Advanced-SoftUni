415ba9910ffcfb034d7abe6e1a668255fc703050
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-13 10:48:46 +0300

    Add files via upload

---
+++ FunctionalProgramming/FunctionalProgramming/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = number => number % 2 == 0;
+
+            List<int> numbers = new List<int>();
+            int[] range = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                numbers.Add(i);
+            }
+
+            string evenOdd = Console.ReadLine();
+
+            if (evenOdd == "even")
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => isEven(x))));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => !isEven(x))));
+            }
+        }
+    }
+}
