212845f6d15da52681eba8316c3f265564dd220c
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-13 10:49:07 +0300

    Add files via upload

---
+++ FunctionalProgramming/5.AppliedArithmetics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                if (input == "add")
+                {
+                    for (int i = 0; i < nums.Count(); i++)
+                    {
+                        nums[i]++;
+                    }
+                }
+                else if (input == "multiply")
+                {
+                    for (int i = 0; i < nums.Count(); i++)
+                    {
+                        nums[i]*=2;
+                    }
+                }
+                else if (input == "subtract")
+                {
+                    for (int i = 0; i < nums.Count(); i++)
+                    {
+                        nums[i]--;
+                    }
+                }
+                else if (input== "print")
+                {
+                    Console.WriteLine(string.Join(" ",nums));
+                }
+
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
