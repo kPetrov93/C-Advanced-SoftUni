3218fc6c98f97ddb2a2a469509f6856fb4d673e3
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-14 10:30:29 +0300

    Add files via upload

---
+++ FunctionalProgramming/8.ListOfPredicates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _8.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 1; i <= n; i++)
+            {
+                bool isDevisible = true;
+                for (int j = 0; j < numbers.Count(); j++)
+                {
+                    if (i % numbers[j] == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        isDevisible = false;
+                    }
+                }
+                if (isDevisible)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
