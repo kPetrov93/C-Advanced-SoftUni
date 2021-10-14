41ad0c267b02b8565cc7a14c8758ce1a8f8c1106
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-10-14 10:30:06 +0300

    Add files via upload

---
+++ FunctionalProgramming/7.PredicateForNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            foreach (var item in names)
+            {
+                if (item.Length<=n)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
