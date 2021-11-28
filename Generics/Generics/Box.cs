9e860df73f818a74b8ff570dd13a272b4c4b25d8
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2021-11-28 09:33:52 +0200

    Add files via upload

---
+++ Generics/Generics/Box.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            T current = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = current;
+        }
+
+        public int CountGreaterThan(T item)
+        {
+            int count = 0;
+
+            foreach (var currentItem in items)
+            {
+                if (currentItem.CompareTo(item) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                string value = item.GetType().ToString();
+                stringBuilder.AppendLine($"{value}: {item}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        } 
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<double> box = new Box<double>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+            double itemToCompare = double.Parse(Console.ReadLine());
+            int result = box.CountGreaterThan(itemToCompare);
+
+            Console.WriteLine(result);
+        }
+    }
+}
